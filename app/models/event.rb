a754082ae32f331cd820333b4c17ea8c681a11b9
Henry van Wagenberg <henryvw1@gmail.com>
2020-01-09 17:47:30 +0100

    Write outermost class method for next 7 day availabilty

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  def self.availabilities(customers_date)
+    7.times.map do |day|
+      next_day = (customers_date + day)
+      { date: next_day.strftime('%Y/%m/%d'),
+        slots: find_available_slots(next_day)
+      }
+    end
+  end
 end
