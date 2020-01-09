5aa7732709098ac454bddb62902703a636fdfc27
Henry van Wagenberg <henryvw1@gmail.com>
2020-01-09 17:44:45 +0100

    Add time cop

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,6 +3,7 @@ require './test/test_helper'
 class EventTest < ActiveSupport::TestCase
   test "one simple test example" do
     test_time = DateTime.parse("2014-08-01 00:00")
+    Timecop.freeze(test_time)
 
     Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
     Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
