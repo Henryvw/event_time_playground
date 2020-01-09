a840e98046c25c6aed0f715ceb152c4e4606345e
Henry van Wagenberg <henryvw1@gmail.com>
2020-01-09 18:06:46 +0100

    Add scope for next 7 days

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@
 class Event < ApplicationRecord
   scope :openings, -> { where(kind: 'opening') }
   scope :appointments, -> { where(kind: 'appointment') }
+  scope :next_n_days, ->(given_day, n_days_in_future) { where('starts_at > ? AND starts_at < ?', given_day, n_days_in_future) }
 
   def self.availabilities(customers_date)
     Array.new(7) do |day|
